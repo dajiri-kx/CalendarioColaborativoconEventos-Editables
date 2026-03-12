3702594c5a92664288809c0865fdee119005e243
DaSilvaCam <camiladasilvafallas25@gmail.com>
2026-03-11 19:20:23 -0600

    crear evento

--- CalendarioColaborativo/WebApplication1/obj/Release/net10.0/WebApplication1.AssemblyInfo.cs
+++ CalendarioColaborativo/WebApplication1/obj/Release/net10.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ef78f1ca98e561450f93dff3f6976c1deb6f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
